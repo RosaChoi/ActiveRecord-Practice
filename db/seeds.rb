c589244a0fe182b4fd7815450074509d1fe1a8f1
waselrosa <rosa@waselrosa.com>
2014-11-26 13:52:34 -0800

    created database using seed.rb and create method for 9 individuals.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+
+people = Person.create([
+  {
+    first_name: "Philip",
+    last_name: "Fry",
+    eye_color: "black",
+    date_of_birth: Date.new(1974,8,14),
+    height_in_inches: 72,
+    awesome: false,
+  },
+  {
+    first_name: "Turanga",
+    last_name: "Leela",
+    eye_color: "black",
+    date_of_birth: Date.new(2974,12,3),
+    height_in_inches: 70,
+    awesome: true,
+  },
+  {
+    first_name: "Hubert",
+    last_name: "Fransworth",
+    eye_color: "green",
+    date_of_birth: Date.new(2841,4,9),
+    height_in_inches: 64,
+    awesome: true,
+  },
+     ])
